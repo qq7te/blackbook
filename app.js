52a06351e2f4c64e502958fbe58ae527d7abedd7
Daniele De Francesco <github@qq7te.com>
2023-02-26 17:39:23 -0500

    now does not need index.html in the URL

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var itemsRouter = require('./routes/items');
 
@@ -42,7 +41,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/', express.static('./bb/build'));
 app.use('/users', usersRouter);
 app.use('/items', itemsRouter);
